813610cb3dbf2652c28b3e8bd03aa11f9245657a
RoberMP <robermontes93@gmail.com>
2021-11-19 12:59:27 +0100

    commit

--- Interfaces/src/Layouts/VGridBagLayout.java
+++ Interfaces/src/Layouts/VGridBagLayout.java
@@ -1,9 +1,67 @@
 package Layouts;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class VGridBagLayout {
 
 	public static void main(String[] args) {
 
+		VentanaGbl2 ventana = new VentanaGbl2();
+		ventana.setVisible(true);
+
 	}
 
+}
+
+class VentanaGbl2 extends JFrame {
+	public VentanaGbl2() {
+		super("Ventana con GBagLayout");
+		setSize(400, 300);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		GridBagLayout gb = new GridBagLayout();
+		GridBagConstraints gbc = new GridBagConstraints();
+
+		setLayout(gb);
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weightx = 1.0;
+
+		JButton boton0 = new JButton("Botón 0"), boton1 = new JButton("Botón 1"), boton2 = new JButton("Botón 2"),
+				boton3 = new JButton("Botón 3"), boton4 = new JButton("Botón 4"), boton5 = new JButton("Botón 5"),
+				boton6 = new JButton("Botón 6"), boton7 = new JButton("Botón 7");
+
+		gb.setConstraints(boton0, gbc);
+		add(boton0);
+		gb.setConstraints(boton1, gbc);
+		add(boton1);
+		gb.setConstraints(boton2, gbc);
+		add(boton2);
+
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+
+		gb.setConstraints(boton3, gbc);
+		add(boton3);
+
+		gbc.weightx = 0.0;
+
+		gb.setConstraints(boton4, gbc);
+		add(boton4);
+		gbc.gridwidth = GridBagConstraints.RELATIVE;
+
+		gb.setConstraints(boton5, gbc);
+		add(boton5);
+
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+
+		gb.setConstraints(boton6, gbc);
+		add(boton6);
+
+		gb.setConstraints(boton7, gbc);
+		add(boton7);
+	}
 }
---
+++ Interfaces/src/Layouts/VGridBagLayout2.java
@@ -0,0 +1,57 @@
+package Layouts;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class VGridBagLayout2 {
+
+	public static void main(String[] args) {
+		VentanaGbl3 ventana = new VentanaGbl3();
+		ventana.setVisible(true);
+	}
+
+}
+
+class VentanaGbl3 extends JFrame {
+	GridBagLayout gb = new GridBagLayout();
+
+	public VentanaGbl3() {
+		super("Ventana con GBagLayout");
+		setSize(400, 300);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JLabel lb1 = new JLabel("Nombre"), lb2 = new JLabel("Apellidos");
+
+		JTextField tx1 = new JTextField(20), tx2 = new JTextField(20);
+
+		JButton bt1 = new JButton("Aceptar");
+
+		getContentPane().setLayout(gb);
+
+		ponComponente(lb1, 0, 0, 1, 1);
+		ponComponente(tx1, 1, 0, 2, 1);
+		ponComponente(lb2, 0, 1, 1, 1);
+		ponComponente(tx2, 2, 1, 1, 1);
+		ponComponente(bt1, 2, 2, 1, 1);
+
+	}
+
+	void ponComponente(JComponent comp, int gridx, int gridy, int gridw, int gridh) {
+		GridBagConstraints gbc = new GridBagConstraints();
+
+		gbc.gridx = gridx;
+		gbc.gridy = gridy;
+		gbc.gridwidth = gridw;
+		gbc.gridheight = gridh;
+
+		gb.setConstraints(comp, gbc);
+		add(comp);
+	}
+}
